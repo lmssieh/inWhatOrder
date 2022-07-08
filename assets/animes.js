18e3d42edad406333ca4f31f2d2a773948d0c6ad
rust <luciferthe79@gmail.com>
2022-07-08 01:29:43 +0100

    feat(order): add steings gate order

---
+++ assets/animes.js
@@ -0,0 +1,18 @@
+const animes = [
+	{
+		name: "danmachi",
+		names: [
+			"Danmachi",
+			"Is It Wrong to Try to Pick Up Girls in a Dungeon?",
+			"Dungeon ni Deai wo Motomeru no wa Machigatteiru Darou ka",
+		],
+		fileName: "danmachi",
+	},
+	{
+		name: "Steins;Gate",
+		names: ["Steins;Gate"],
+		fileName: "steins;gate",
+	},
+];
+
+export default animes;
