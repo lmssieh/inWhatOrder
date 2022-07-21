1cd9a4a5728e7ae64fac38e109ab42e65dd14811
rust <53446488+lmssieh@users.noreply.github.com>
2022-07-21 16:48:00 +0100

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Order from "./routes/Order";
+import FetchOrder from "./components/FetchOrder";
+import Invoice from "./components/Invoice";
 import "virtual:windi.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="order" element={<Order />}>
+						<Route path=":anime" element={<FetchOrder />} />
+					</Route>
+				</Route>
+				<Route
+					path="*"
+					element={
+						<main style={{ padding: "1rem" }}>
+							<p>There's nothing here!</p>
+						</main>
+					}
+				/>
+			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
 );
